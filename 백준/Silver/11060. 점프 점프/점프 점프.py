dcdad726aa27dcb5d9051c1dcee2546a12b088e6
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-03-29 22:21:27 +0900

    [Silver II] Title: 점프 점프, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -3,10 +3,9 @@ num = [0] + list(map(int, input().split()))
 dp = [0, 1]+[1000]*(n-1)  # 인덱스가 좌표가 되도록, 시작점은 1
 i = 1
 while i<n:
-    if dp[i]:
-        for move in range(1, num[i]+1):  # 현재 위치에서 점프 가능한 곳들을 +1 해준다. 
-            if i+move <= n:
-                dp[i+move] = min(dp[i+move], dp[i]+1)
+    for move in range(1, num[i]+1):  # 현재 위치에서 점프 가능한 곳들을 +1 해준다. 
+        if i+move <= n:
+            dp[i+move] = min(dp[i+move], dp[i]+1)
     i += 1
 if dp[n]==1000:
     print(-1)    
