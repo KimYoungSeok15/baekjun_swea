fc7ba74cc3428c1dee50eafda248a596aac2f2e3
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-04-19 22:06:10 +0900

    [Gold V] Title: 센서, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -1,11 +1,11 @@
 n = int(input())
 k = int(input())
 lst = sorted(list(map(int, input().split())))
+if k == 1:
+    print(max(lst)-min(lst))
+    exit()
 dist = []
 for i in range(n-1):
     dist.append(lst[i+1]-lst[i])
-
-while k > 1 and dist:
-    dist.remove(max(dist))
-    k -= 1
-print(sum(dist))
+dist.sort()
+print(sum(dist[:-k+1]))
