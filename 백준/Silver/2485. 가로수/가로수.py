5f3098b9852f3a8351da6b824014712fa1dc21e7
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-04-22 00:10:32 +0900

    [Silver IV] Title: 가로수, Time: 208 ms, Memory: 124784 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,8 +1,7 @@
-import math
-n = int(input())
+import math, sys
+n = int(sys.stdin.readline())
 lst = [int(input()) for _ in range(n)]
 temp = []
 for i in range(n-1):
     temp.append(lst[i+1] - lst[i])
 print((max(lst) - min(lst))//math.gcd(*temp)-n+1)
-    
