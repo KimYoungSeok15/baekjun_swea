de78f69b934d369a8f0670e937a20c691a421abe
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-07-17 22:10:44 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,40 +2,28 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 n,m,v = map(int, input().split())
-edge = sorted([list(map(int, input().split())) for _ in range(m)])
+edge = [[] for _ in range(n+1)]
+for i in range(m):
+    a,b = map(int,input().split())
+    edge[a].append(b)
+    edge[b].append(a)
+for e in edge:
+    e.sort()
 visited = [0]*(n+1)
-ans = []
-def dfs(s):
-    visited[s] = 1
-    ans.append(s)
-    temp = []
-    for e in edge:
-        if e[0] == s and not visited[e[1]]:
-            temp.append(e[1])
-        elif e[1] == s and not visited[e[0]]:
-            temp.append(e[0])
-    temp.sort()
-    for t in temp:
-        if not visited[t]:
-            dfs(t)
-    
+def dfs(x):
+    visited[x] = 1
+    print(x, end=' ')
+    for n in edge[x]:
+        if not visited[n]:
+            dfs(n)
 dfs(v)
-print(*ans)
-ans = []
-q = deque([v])
+print()
 visited = [0]*(n+1)
+q = deque([v])
 while q:
     now = q.popleft()
-    ans.append(now)
+    if visited[now]:
+        continue
     visited[now] = 1
-    temp = []
-    for e in edge:
-        if e[0] == now and not visited[e[1]]:
-            temp.append(e[1])
-            visited[e[1]] = 1
-        if e[1] == now and not visited[e[0]]:
-            temp.append(e[0])
-            visited[e[0]] = 1
-    temp.sort()
-    q.extend(temp)
-print(*ans)
+    print(now, end=' ')
+    q.extend(edge[now])
