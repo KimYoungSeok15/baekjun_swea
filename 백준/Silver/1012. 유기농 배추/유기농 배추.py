ed5abab2513ec415273c57372d424989ad6980df
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-07-26 21:25:06 +0900

    [Silver II] Title: 유기농 배추, Time: 328 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,24 @@
+from collections import deque
+t = int(input())
+for tc in range(t):
+    m, n, k = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(k)]
+    board = [[0]*n for _ in range(m)] 
+    visited = [[0]*n for _ in range(m)]
+    d = ((0,1),(0,-1),(1,0),(-1,0))
+    ans = 0
+    for cab in arr:
+        board[cab[0]][cab[1]] = 1
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] and not visited[i][j]:
+                ans += 1
+                q = deque([(i,j)])
+                while q:
+                    om, on = q.popleft()
+                    for dm, dn in d:
+                        nm, nn = om+dm, on+dn
+                        if 0<=nm<m and 0<=nn<n and not visited[nm][nn] and board[nm][nn]:
+                            visited[nm][nn] = 1
+                            q.append((nm, nn))
+    print(ans)
