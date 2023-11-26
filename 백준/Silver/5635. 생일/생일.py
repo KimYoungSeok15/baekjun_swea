4e6f3880ec935abe16a75ee39c01f9c9cbeceddf
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2023-11-27 00:14:30 +0900

    [Silver V] Title: 생일, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -1,11 +1,12 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+arr = []
+for _ in range(n):
+    n, d, m, y = input().split()
+    d, m, y = map(int, (d, m, y))
+    arr.append([y,m,d,n])
+arr.sort()
+print(arr[-1][3])
+print(arr[0][3])
 
-lst = []
-for _ in range(int(input())):
-    n,d,m,y = input().rstrip().split()
-    d,m,y = map(int,(d,m,y))
-    lst.append((y,m,d,n))
-lst.sort()
-print(lst[-1][3])
-print(lst[0][3])
+
+    
