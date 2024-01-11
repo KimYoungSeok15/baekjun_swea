d616fa9b8bd5754b81885440335e1087e2990caa
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2024-01-11 23:04:44 +0900

    [Silver III] Title: 시리얼 번호, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -1,7 +1,5 @@
 i=input
 g=[i() for _ in range(int(i()))]
-g.sort()
-g.sort(key=lambda x:sum([int(j) if j.isnumeric() else 0 for j in x]))
-g.sort(key=lambda x:len(x))
+g.sort(key=lambda x:(len(x),sum([int(j) if j.isnumeric() else 0 for j in x]),x))
 for k in g:
   print(k)
