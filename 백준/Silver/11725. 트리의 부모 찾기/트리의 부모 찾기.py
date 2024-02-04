9186d2596fc1173640d35171c63275d7c37961fd
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2024-02-04 23:14:20 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 288 ms, Memory: 127492 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,27 +1,21 @@
-import sys
-from collections import deque
+from collections import deque as d
+n = int(input())
+edge = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    edge[a].append(b)
+    edge[b].append(a)
+    
+parent = [0]*(n+1)
 
-N = int(sys.stdin.readline())
-graph = [[] for i in range(N+1)]
-for _ in range(N-1):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-queue = deque()
-queue.append(1)
-
-ans = [0]*(N+1)
-
-def bfs():
-    while queue:
-        now = queue.popleft()
-        for nxt in graph[now]:
-            if ans[nxt] == 0:
-                ans[nxt] = now
-                queue.append(nxt)
-
-bfs()
-res = ans[2:]
-for x in res:
-    print(x)
+q = d([1])
+v = [0]*(n+1)
+parent[1] = 1
+while q:
+    now = q.popleft()
+    for e in edge[now]:
+        if not parent[e]:
+            q.append(e)
+            parent[e] = now
+for i in range(2, n+1):
+    print(parent[i])
