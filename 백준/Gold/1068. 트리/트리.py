3d25acd4c1a0b6cd683e8cd5c3ecebcf2c793afd
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2024-02-04 23:49:08 +0900

    [Gold V] Title: 트리, Time: 136 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,30 +1,37 @@
 from collections import deque
-
 n = int(input())
-lst = list(map(int, input().split()))
+parent = list(map(int, input().split()))
 d = int(input())
+for idx, val in enumerate(parent):
+    if val == -1:
+        root = idx
+        break
+
+leaf_possible = [1]*n
+for i in range(n):
+    if i != root:
+        leaf_possible[parent[i]] = 0
+if n == 1:
+    leaf_possible[0] = 1
+if parent.count(parent[d]) == 1:
+    leaf_possible[parent[d]] = 1
+
+def is_root(now):
+    if now == d:
+        return False
+    while True:
+        if parent[now] == d:
+            return False
+        elif parent[now] == root:
+            return True
+        else:
+            now = parent[now]
 
 ans = 0
-
-for i in range(n):
-    if lst[i] == -1:
-        root = i
-        break
- 
-if root == d:
-    print(0)
-    exit()    
-    
-q = deque([root])
-
-while q:
-    now = q.popleft()
-    is_leaf = True
-    for i in range(n):
-        if lst[i] == now and i != d:
-            q.append(i)
-            is_leaf = False
-    if is_leaf:
-        ans += 1
-
+for idx in range(n):
+    if leaf_possible[idx]:
+        if is_root(idx):
+            ans += 1
+            
 print(ans)
+    
