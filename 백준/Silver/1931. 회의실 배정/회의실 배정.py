ab9a6d98c3ad55253dd89381222d9601171dd79d
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2024-03-05 22:07:42 +0900

    [Silver I] Title: 회의실 배정, Time: 356 ms, Memory: 121876 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+meet = [list(map(int, input().split())) for _ in range(n)]
+meet.sort()
+ans = 0
+i = 0
+after = -1
+while i < n:
+    s, e = meet[i]
+    if s >= after:
+        ans += 1
+        after = e
+
+    elif e <= after:
+        after = e
+
+    i += 1
+
+
+print(ans)
