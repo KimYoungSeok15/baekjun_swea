c6f00ffd0e4699e376326906136ed63f052df996
KimYoungSeok15 <122508517+KimYoungSeok15@users.noreply.github.com>
2024-04-27 23:56:27 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 212 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,22 +1,40 @@
-from itertools import permutations
-
-n = int(input())
-arr = list(map(int, input().split()))
+N = int(input())
+nums = list(map(int, input().split()))
 oper = list(map(int, input().split()))
-lst = ["+"] * oper[0] + ["-"] * oper[1] + ["*"] * oper[2] + ["/"] * oper[3]
+
 max_ans, min_ans = -int(1e9), int(1e9)
-for comb in permutations(lst):
-    ans = arr[0]
-    for i in range(n - 1):
-        if comb[i] == "+":
-            ans += arr[i + 1]
-        elif comb[i] == "-":
-            ans -= arr[i + 1]
-        elif comb[i] == "*":
-            ans *= arr[i + 1]
-        else:
-            ans = int(ans / arr[i + 1])
-    max_ans = max(max_ans, ans)
-    min_ans = min(min_ans, ans)
+
+
+def dfs(depth, max_now, min_now):
+    global max_ans, min_ans
+
+    if depth == N:
+        max_ans = max(max_ans, max_now)
+        min_ans = min(min_ans, min_now)
+        return
+
+    if oper[0]:
+        oper[0] -= 1
+        dfs(depth + 1, max_now + nums[depth], min_now + nums[depth])
+        oper[0] += 1
+
+    if oper[1]:
+        oper[1] -= 1
+        dfs(depth + 1, max_now - nums[depth], min_now - nums[depth])
+        oper[1] += 1
+
+    if oper[2]:
+        oper[2] -= 1
+        dfs(depth + 1, max_now * nums[depth], min_now * nums[depth])
+        oper[2] += 1
+
+    if oper[3]:
+        oper[3] -= 1
+        dfs(depth + 1, int(max_now / nums[depth]), int(min_now / nums[depth]))
+        oper[3] += 1
+
+
+dfs(1, nums[0], nums[0])
+
 print(max_ans)
 print(min_ans)
